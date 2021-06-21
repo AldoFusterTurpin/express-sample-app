5eba9ce6168384a1ccec2945a36f43084a1eba75
Aldo <aldoft@hotmail.com>
2021-06-21 13:19:49 +0200

    Personalize app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Aldo and JS are friends... But I prefer Go or Python 🌚' });
 });
 
 module.exports = router;
